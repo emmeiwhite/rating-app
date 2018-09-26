d2a76a699507c46eaf69fb0c451e777de3ff9703
emmeiwhite <emmeiwhite@gmail.com>
2018-09-26 17:35:04 +0530

    Working for one try only - need to make state changes

--- src/components/questions.js
+++ src/components/questions.js
@@ -9,7 +9,8 @@ export default class Questions extends Component{
 
         this.state = {
             questions:[],
-            smileyResponses:["1.Most Disliked","2.Dislike","3.Like","4.Brillient Response","5.Outstanding Response"],
+            // smileyResponses:["1.Most Disliked","2.Dislike","3.Like","4.Brillient Response","5.Outstanding Response"], as of now I see no use of this state
+            catalist:false,
             buttonIds:[1,2,3,4,5]
         }
     }
@@ -39,22 +40,47 @@ export default class Questions extends Component{
                 switch(btnSelected){
                     case 1:
                         document.getElementById(selectedQuestion).innerHTML+="1.Most Disliked";
+                        let catalistDemo1= this.state.catalist;
+                        this.setState({
+                            ...this.state,
+                            catalist:!catalistDemo1
+                        })
                     break;
 
                     case 2:
                         document.getElementById(selectedQuestion).innerHTML+="2.Dislike";
+                        let catalistDemo2 = this.state.catalist;
+                        this.setState({
+                            ...this.state,
+                            catalist:!catalistDemo2
+                        })
                     break;
 
                     case 3:
                         document.getElementById(selectedQuestion).innerHTML+="3.Like";
+                        let catalistDemo3 = this.state.catalist;
+                        this.setState({
+                            ...this.state,
+                            catalist:!catalistDemo3
+                        })
                     break;
 
                     case 4:
                         document.getElementById(selectedQuestion).innerHTML+="4.Brillient Response";
+                        let catalistDemo4 = this.state.catalist;
+                        this.setState({
+                            ...this.state,
+                            catalist:!catalistDemo4
+                        })
                     break;
 
                     case 5:
                         document.getElementById(selectedQuestion).innerHTML+="5.Outstanding Response";
+                        let catalistDemo5 = this.state.catalist;
+                        this.setState({
+                            ...this.state,
+                            catalist:!catalistDemo5
+                        })
                     break;
                 }
             break;
