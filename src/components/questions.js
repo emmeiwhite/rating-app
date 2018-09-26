47cc796b6cbc6f5a4b2dc88b6eec609353de9414
emmeiwhite <emmeiwhite@gmail.com>
2018-09-26 16:56:21 +0530

    App starting to REACT - and this is REACT :(

--- src/components/questions.js
+++ src/components/questions.js
@@ -9,7 +9,7 @@ export default class Questions extends Component{
 
         this.state = {
             questions:[],
-            smileyResponses:["1.Greater Dislike","2.Dislike","3.Like","4.Brilient Response","5.Outstanding Response"],
+            smileyResponses:["1.Most Disliked","2.Dislike","3.Like","4.Brillient Response","5.Outstanding Response"],
             buttonIds:[1,2,3,4,5]
         }
     }
@@ -28,27 +28,153 @@ export default class Questions extends Component{
         })
     }
 
-    showSmileyMessage = (selectedQuestion,)=>{
-        console.log(selectedQuestion);
+    showSmileyMessage = (selectedQuestion,btnSelected)=>{
+        console.log(selectedQuestion,btnSelected);
+        switch(selectedQuestion){
+            case 0:
+                switch(btnSelected){
+                    case 1:
+                        document.getElementById(selectedQuestion).innerHTML+="1.Most Disliked";
+                    break;
+
+                    case 2:
+                        document.getElementById(selectedQuestion).innerHTML+="2.Dislike";
+                    break;
+
+                    case 3:
+                        document.getElementById(selectedQuestion).innerHTML+="3.Like";
+                    break;
+
+                    case 4:
+                        document.getElementById(selectedQuestion).innerHTML+="4.Brillient Response";
+                    break;
+
+                    case 5:
+                        document.getElementById(selectedQuestion).innerHTML+="5.Outstanding Response";
+                    break;
+                }
+            break;
+
+            case 1:
+                switch(btnSelected){
+                    case 1:
+                    document.getElementById(selectedQuestion).innerHTML+="1.Most Disliked";
+                    break;
+
+                    case 2:
+                        document.getElementById(selectedQuestion).innerHTML+="2.Dislike";
+                    break;
+
+                    case 3:
+                        document.getElementById(selectedQuestion).innerHTML+="3.Like";
+                    break;
+
+                    case 4:
+                        document.getElementById(selectedQuestion).innerHTML+="4.Brillient Response";
+                    break;
+
+                    case 5:
+                        document.getElementById(selectedQuestion).innerHTML+="5.Outstanding Response";
+                    break;
+                }
+            break;
+
+            case 2:
+                switch(btnSelected){
+                    case 1:
+                        document.getElementById(selectedQuestion).innerHTML+="1.Most Disliked";
+                    break;
+
+                    case 2:
+                        document.getElementById(selectedQuestion).innerHTML+="2.Dislike";
+                    break;
+
+                    case 3:
+                        document.getElementById(selectedQuestion).innerHTML+="3.Like";
+                    break;
+
+                    case 4:
+                        document.getElementById(selectedQuestion).innerHTML+="4.Brillient Response";
+                    break;
+
+                    case 5:
+                        document.getElementById(selectedQuestion).innerHTML+="5.Outstanding Response";
+                    break;
+                }
+            break;
+
+            case 3:
+                switch(btnSelected){
+                    case 1:
+                        document.getElementById(selectedQuestion).innerHTML+="1.Most Disliked";
+                    break;
+
+                    case 2:
+                        document.getElementById(selectedQuestion).innerHTML+="2.Dislike";
+                    break;
+
+                    case 3:
+                        document.getElementById(selectedQuestion).innerHTML+="3.Like";
+                    break;
+
+                    case 4:
+                        document.getElementById(selectedQuestion).innerHTML+="4.Brillient Response";
+                    break;
+
+                    case 5:
+                        document.getElementById(selectedQuestion).innerHTML+="5.Outstanding Response";
+                    break;
+                }
+            break;
+
+            case 4:
+                switch(btnSelected){
+                    case 1:
+                        document.getElementById(selectedQuestion).innerHTML+="1.Most Disliked";
+                    break;
+
+                    case 2:
+                        document.getElementById(selectedQuestion).innerHTML+="2.Dislike";
+                    break;
+
+                    case 3:
+                        document.getElementById(selectedQuestion).innerHTML+="3.Like";
+                    break;
+
+                    case 4:
+                        document.getElementById(selectedQuestion).innerHTML+="4.Brillient Response";
+                    break;
+
+                    case 5:
+                        document.getElementById(selectedQuestion).innerHTML+="5.Outstanding Response";
+                    break;
+                }
+            break;
+
+            default:
+                console.log("The Default Case");
+
+
+        }
+
     }
 
     render(){
         return(
                   this.state.questions.map((question,index)=>{
                         return (
-                        <div className="row" key={question.id}>
+                        <div className="row" key={question.id} id={index}>
                                 <div  className="questions" className="col-md-8">
                                     <p>{question.question}</p>
-                                    <Button clickEvent={this.showSmileyMessage.bind(this,index)}>{this.state.buttonIds[0]}</Button>
-                                    <Button clickEvent={this.showSmileyMessage.bind(this,index)}>{this.state.buttonIds[1]}</Button>
-                                    <Button clickEvent={this.showSmileyMessage.bind(this,index)}>{this.state.buttonIds[2]}</Button>
-                                    <Button clickEvent={this.showSmileyMessage.bind(this,index)}>{this.state.buttonIds[3]}</Button>
-                                    <Button clickEvent={this.showSmileyMessage.bind(this,index)}>{this.state.buttonIds[4]}</Button>
-                                 
+                                    <Button clickEvent={this.showSmileyMessage.bind(this,(index),1)}>{this.state.buttonIds[0]}</Button>
+                                    <Button clickEvent={this.showSmileyMessage.bind(this,(index),2)}>{this.state.buttonIds[1]}</Button>
+                                    <Button clickEvent={this.showSmileyMessage.bind(this,(index),3)}>{this.state.buttonIds[2]}</Button>
+                                    <Button clickEvent={this.showSmileyMessage.bind(this,(index),4)}>{this.state.buttonIds[3]}</Button>
+                                    <Button clickEvent={this.showSmileyMessage.bind(this,(index),5)}>{this.state.buttonIds[4]}</Button>   
                                 </div>
 
-                                <div className="col-md-4">
-                                    <Smileys />
+                                <div className="col-md-4"  >
+                                    {/* <Smileys /> */}
                                 </div>
                         </div>
                        );
